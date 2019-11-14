f82059f1a9c14574852e92185840fff455b5ad0a
snOm3ad <geronimooliver00@gmail.com>
2019-11-14 12:28:59 -0500

    fixed cors problems
    it took 3 hours to fix this... the issue is that browsers do not allow
    fetch calls to external servers unless the headers of the requested
    resource have the `AccessControlAllowOrigin` header.
    
    I still have to research this more, in order to understand how to
    properly manage this property. for now the quick-fix is to allow
    requests from anywhere.

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,10 @@ module github.com/snOm3ad/flashcard-app-backend
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/kr/pretty v0.1.0 // indirect
+	golang.org/x/sys v0.0.0-20190507160741-ecd444e8653b // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "database/sql"
+    _ "database/sql"
+    "net/http"
     "github.com/gin-gonic/gin"
 )
 
@@ -20,9 +20,26 @@ type (
     }
 )
 
+
+// Let's start by creating an in-memory server of the data.
 func ServeContent(courseID int32) func (*gin.Context) {
+    // We will need a list of `FlashCard` items which contains 
+    var flashCards []FlashCard;
+
+
+    // Now we will append things to the list
+    flashCards = append(flashCards, FlashCard {
+        Answer: "## Answer\nThis is an example answer, answers in *Flashcards* allow for `markdown`-syntax which allows you to write expressive answers.\n- Example 1\n- Example 2",
+        Question: FlashCardQuestion {
+            Index: 1,
+            Content: "This is an example question?",
+            Time: 30.0,
+            Score: 100,
+        },
+    });
 
     return func (c *gin.Context) {
+        c.JSON(http.StatusOK, flashCards);
     }
 }
 
@@ -31,11 +48,19 @@ func Init() {
 
 func main() {
     router := gin.Default()
-    router.GET("/", func (ctx *gin.Context) {
-        fmt.Fprintln(ctx.Writer, "Hello World")
-    })
-
+    router.Use(CORSMiddleware());
     // Here we would write something like.
+    router.GET("/courses/indu/412", ServeContent(412))
+    router.Run(":8080")
+}
 
-    router.Run()
+
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+        c.Next()
+    }
 }
