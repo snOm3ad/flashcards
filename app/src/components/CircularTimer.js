da79af049fc9bb17ca35ffd42c6d62eee8076c90
snOm3ad <geronimooliver00@gmail.com>
2019-11-16 16:01:14 -0500

    fixed memory leak bug
    inside the `CircularTimer` component two `useEffect`
    calls where made:
    
        1. read the `isMounted.current` property
        2. change the value of `isMounted.current`
    
    this lead to a problem because React relies on the order
    in which hooks are called. which means that sometimes the
    read being done in (1) was outdated, which lead to a memory
    leak because it was mutating the state after the component
    had been unmounted.

--- app/src/components/CircularTimer.js
+++ app/src/components/CircularTimer.js
@@ -4,43 +4,44 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 function CircularTimer({ finished, time, setTime, timeLimit }) {
-  const [value, setValue] = useState(100);
-  const isMounted = useRef(true);
+    const [value, setValue] = useState(100);
+    const isMounted = useRef(true);
+    
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
 
-  useEffect(() => {
-    // run this only if the user has not asked for the answer
-    // to the question. We don't care if the timer has run out
-    // or not.
-    if (!finished) {
-      setTimeout(() => {
-        if (isMounted.current && time > 0) {
-          setTime(time - 1);
+    useEffect(() => {
+        // run this only if the user has not asked for the answer
+        // to the question. We don't care if the timer has run out
+        // or not.
+        if (!finished && time > 0) {
+            setTimeout(() => {
+                if (isMounted.current) {
+                    console.log(`isMounted: ${isMounted.current}`);
+                    setTime(time - 1);
+                }
+            }, 1000);
+
+            if (isMounted.current) {
+                setValue(Math.floor(((time - 1) / timeLimit) * 100));
+            }
         }
-      }, 1000);
+    }, [time, timeLimit, setTime, setValue, finished]);
 
-      if (isMounted.current && time > 0) {
-        setValue(Math.floor(((time - 1) / timeLimit) * 100));
-      }
-    }
-  }, [time, timeLimit, setTime, setValue, finished]);
-
-  useEffect(() => {
-    return () => {
-      isMounted.current = false;
-    };
-  }, []);
-
-  return (
-    <CircularProgressbar
-      value={value}
-      strokeWidth={20}
-      styles={buildStyles({
-        strokeLinecap: "butt",
-        pathColor: value > 25 ? "white" : "red",
-        trailColor: "#234C63"
-      })}
-    />
-  );
+    return (
+        <CircularProgressbar
+            value={value}
+            strokeWidth={20}
+            styles={buildStyles({
+                strokeLinecap: "butt",
+                pathColor: value > 25 ? "white" : "red",
+                trailColor: "#234C63"
+            })}
+        />
+    );
 }
 
 export default CircularTimer;
